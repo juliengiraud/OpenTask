2821e76cb10c59bfd9c6879de8c4a2a5839c0960
ju <giraud740@gmail.com>
2026-05-14 15:29:09 +0200

    initial commit

---
+++ app/src/main/java/com/example/androidtaskapp/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.androidtaskapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import com.example.androidtaskapp.ui.theme.AndroidTaskAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidTaskAppTheme {
+                AndroidTaskAppApp()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun AndroidTaskAppApp() {
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            painterResource(it.icon),
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            val modifier = Modifier.padding(innerPadding)
+            when (currentDestination) {
+                AppDestinations.HOME -> Greeting("Home", modifier)
+                AppDestinations.FAVORITES -> Greeting("Favorites", modifier)
+                AppDestinations.PROFILE -> Greeting("Profile", modifier)
+            }
+        }
+    }
+}
+
+enum class AppDestinations(
+    val label: String,
+    val icon: Int,
+) {
+    HOME("Home", R.drawable.ic_home),
+    FAVORITES("Favorites", R.drawable.ic_favorite),
+    PROFILE("Profile", R.drawable.ic_account_box),
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidTaskAppTheme {
+        Greeting("Android")
+    }
+}
